0ef0a86bcd1e069c3014052994c7e933d4091357
Justin Frost <frostjust@gmail.com>
2018-06-03 19:33:50 -0700

    Initial card clicking logic

--- js/app.js
+++ js/app.js
@@ -1,7 +1,32 @@
 $(function() {
-  $(".card").click(function() {
-    console.log("Click!");
-    $(".card-back").toggleClass("rotate-card-back");
-    $(".card-front").toggleClass("rotate-card-front");
-  })
+  var registerEventListeners = function() {
+    deck.click(function(e) {
+      var target = $(e.target);
+      var id = target.attr("id");
+      var elClass = target.attr("class");
+
+      console.log("Click: " + id + " " + target.attr("class"));
+      
+      if (elClass === "card-back") {
+        target.toggleClass("rotate-card-back");
+        target.next(".card-front").toggleClass("rotate-card-front");
+      }
+    });
+  };
+
+  var newCardId = function(n) {
+    return "card-" + (n < 10 ? "0" : "") + n.toString();
+  };
+
+  var deck = $(".card-deck");
+  var card = $("#card-template .rotate-container");
+  var newCard;
+
+  for (var i = 0; i < 4; i++) {
+    newCard = card.clone();
+    newCard.find(".card-back").prop("id", newCardId(i));
+    newCard.appendTo(deck);
+  }
+
+  registerEventListeners();
 });
